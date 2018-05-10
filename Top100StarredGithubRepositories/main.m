01e08c07afcabd4b50ab3d838cc824d99944cbda
Andrea Fletcher <acfletch53@gmail.com>
2018-05-09 17:34:05 -0700

    Initial Commit

---
+++ Top100StarredGithubRepositories/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Top100StarredGithubRepositories
+//
+//  Created by Andrea Fletcher on 5/9/18.
+//  Copyright © 2018 Andrea Fletcher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
